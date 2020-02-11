64fb0fcca455cda2f48ac52a1a7086d482aa7f18
tombrink8271 <48510211+tombrink8271@users.noreply.github.com>
2020-02-11 15:38:44 +0100

    The ADDS tools DSC script

---
+++ ProvisionSRVINT01.ps1
@@ -0,0 +1,17 @@
+﻿Configuration SRVINT01Cfg {
+
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    Node 'localhost' {
+
+        # The first resource block ensures that the ADDSTools are   enabled.
+        WindowsFeature ADDSTools {
+            Ensure = "Present"
+            Name   = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+
+
+    }
+}
